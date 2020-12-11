1ca9b02f23c168f149acf80cf3db20d0dd131347
KaViN-DEMON78 <75722797+KaViN-DEMON78@users.noreply.github.com>
2020-12-11 15:47:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var movingRect,fixedRect;
+function setup() {
+  createCanvas(800,400);
+ movingRect= createSprite(400, 200, 50, 50);
+ movingRect.shapeColor="red";
+ fixedRect=createSprite(200,200,100,50)
+ fixedRect.shapeColor="red"
+}
+
+function draw() {
+  background(255,255,255);  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2
+    && fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2
+    && movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2
+    && fixedRect.y - movingRect.y<fixedRect.height/2+movingRect.height/2){
+  movingRect.shapeColor="blue";
+  fixedRect.shapeColor="blue";
+  }
+  else{
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  drawSprites();
+}
